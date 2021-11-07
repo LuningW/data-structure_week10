a9681733b689fb24a1a2ae928460609cd9255de1
Luuuun <93080608+Luuuun@users.noreply.github.com>
2021-11-07 22:33:06 +0800

    Update 不相交集.cpp

--- 不相交集.cpp
+++ 不相交集.cpp
@@ -11,6 +11,24 @@ using namespace std;
 
 int main()
 {
-
+  		int elementNum = 8;
+		DisjSet S;
+		Initilialize(S);
+		SetUnion(S, 5, 6);
+		SetUnion(S, 7, 8);
+		SetUnion(S, 5, 7);
+		/*
+		SetUnion_High(S, 5, 6);
+		SetUnion_High(S, 7, 8);
+		SetUnion_High(S, 5, 7);
+		*/
+		cout << (Find(4,S) == Find(5,S)) << endl;
+		cout << (Find(6,S) == Find(8,S)) << endl;
+		/*
+		cout << (Find_Compression(4, S) == Find_Compression(5, S)) << endl;
+		cout << (Find_Compression(6, S) == Find_Compression(8, S)) << endl;
+		*/
+		system("pause");
+		return 0;
 }
 
